306953278e8bdcc9f14f971e6a01e04ddfab7ea5
stretch1414 <jmoser1414@gmail.com>
2024-08-15 21:16:45 -0400

    Add basic 200 response

--- app/server.go
+++ app/server.go
@@ -16,9 +16,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	n, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing bytes", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Wrote %d bytes in response", n)
+
+	conn.Close()
 }
